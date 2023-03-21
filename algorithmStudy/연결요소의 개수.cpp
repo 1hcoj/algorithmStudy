5e0db3577a484f90455219994d7071311755dd8d
chihyeon <chi2941@naver.com>
2023-03-21 14:09:45 +0900

    초기 커밋.

---
+++ algorithmStudy/연결요소의 개수.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> graph;
+
+void dfs(int v, vector<bool>& visited) {
+	visited[v] = true;
+	for (int v1 : graph[v]) {
+		if (!visited[v1]) dfs(v1, visited);
+	}
+}
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	graph = vector<vector<int>>(n + 1);
+	vector<bool> visited(n + 1, false);
+
+	for (int i = 0; i < m; i++) {
+		int v1, v2;
+		cin >> v1 >> v2;
+		graph[v1].push_back(v2);
+		graph[v2].push_back(v1);
+	}
+	int count = 0;
+	for (int i = 1; i <= n; i++) {
+		if (!visited[i]) {
+			count++;
+			dfs(i, visited);
+		}
+	}
+	cout << count << endl;
+}
