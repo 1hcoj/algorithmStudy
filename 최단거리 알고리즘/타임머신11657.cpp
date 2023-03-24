f20ed927949c44843fdddf19896f1635e0a4f029
chihyeon <chi2941@naver.com>
2023-03-24 22:47:21 +0900

    0324

--- 최단거리 알고리즘/타임머신11657.cpp
+++ 최단거리 알고리즘/타임머신11657.cpp
@@ -5,21 +5,21 @@
 using namespace std;
 
 vector<vector<pair<int, int>>> graph;
-const long long INF = INT64_MAX ;
+const long long INF = INT64_MAX;
 
 
 int main() {
-	int n, m, x,v1,v2,w;
+	int n, m, x, v1, v2, w;
 	cin >> n >> m >> x;
 	graph = vector<vector<pair<int, int>>>(n + 1);
-	vector<long long> distance(n + 1,INF);
+	vector<long long> distance(n + 1, INF);
 	distance[x] = 0;
 
 	for (int i = 0; i < m; i++) {
 		cin >> v1 >> v2 >> w;
 		graph[v1].push_back({ v2,w });
 	}
-	
+
 	vector<bool> visited(n + 1, false);
 
 	priority_queue<pair<int, int>> q;
@@ -38,7 +38,7 @@ int main() {
 			}
 		}
 	}
-	for (int i = 1; i <= n;i++) {
+	for (int i = 1; i <= n; i++) {
 		long long d = distance[i];
 		if (d != INF) cout << d << '\n';
 		else cout << "INF" << '\n';
